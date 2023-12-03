d5a3d5cd229c88f4dae5a5eb0b0a73a92e46c4c9
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-12-02 23:34:22 -0800

    p143 ListNode struct defs

--- p143_reorder_list/src/lib.rs
+++ p143_reorder_list/src/lib.rs
@@ -1,5 +1,21 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
 }
 
 #[cfg(test)]
