8484d12af338076d73744d2368276aa2fd46a935
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-12-03 17:29:22 -0800

    WIP p143

--- p143_reorder_list/src/lib.rs
+++ p143_reorder_list/src/lib.rs
@@ -18,6 +18,27 @@ impl ListNode {
 pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
 }
 
+pub fn reorder_list_linear_memory(head: &mut Option<Box<ListNode>>) {
+    let mut v = Vec::<Option<Box<ListNode>>>::new();
+    while let Some(mut node) = head.take() {
+        *head = node.next.take();
+        v.push(Some(node));
+    }
+    let (mut l, mut r) = (0, v.len() - 1);
+    let mut runner = Some(Box::new(ListNode::new(0)));
+    let mut dummy = ListNode::new(0);
+    dummy.next = runner;
+    while l < r {
+        runner = v[l].take();
+        runner.unwrap().next = v[r].take();
+        l += 1;
+        if l < r {
+
+        }
+    }
+    let _ = head.insert(dummy.next.unwrap());
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
