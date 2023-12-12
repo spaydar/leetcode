d47e57236884e4aa46cc0886ebb441b60bbf7eef
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-12-11 20:41:00 -0800

    feat: create_n_list_nodes + tests

--- utils/src/list_node.rs
+++ utils/src/list_node.rs
@@ -15,17 +15,48 @@ impl ListNode {
   }
 }
 
-pub fn create_n_list_nodes(_n: i32) -> Option<Box<ListNode>> {
-    None
+pub fn create_n_list_nodes(n: i32) -> Option<Box<ListNode>> {
+    let mut i = 1;
+    let mut dummy = Some(Box::new(ListNode::new(0)));
+    let mut r = &mut dummy;
+    while i <= n {
+        r.as_mut().unwrap().next = Some(Box::new(ListNode::new(i)));
+        r = &mut r.as_mut().unwrap().next;
+        i += 1;
+    }
+    dummy.unwrap().next
 }
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn it_works() {
-//        //let result = add(2, 2);
-//        //assert_eq!(result, 4);
-//    }
-//}
+/// Add line `println!("create_n_list_nodes(x) {:#?}", l);` and run `cargo test -- --nocapture`
+/// to view ListNode structure in the console.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_0_list_nodes() {
+        let l = create_n_list_nodes(0);
+        assert_eq!(l, None);
+    }
+
+    #[test]
+    fn create_1_list_nodes() {
+        let l = create_n_list_nodes(1);
+        let expected = Some(Box::new(ListNode { val: 1, next: None }));
+        assert_eq!(l, expected);
+    }
+
+    #[test]
+    fn create_4_list_nodes() {
+        let l = create_n_list_nodes(4);
+        let expected = Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 3, next: Some(Box::new(ListNode { val: 4, next: None })) })) })) }));
+        assert_eq!(l, expected);
+    }
+
+    #[test]
+    fn create_5_list_nodes() {
+        let l = create_n_list_nodes(5);
+        let expected = Some(Box::new(ListNode { val: 1, next: Some(Box::new(ListNode { val: 2, next: Some(Box::new(ListNode { val: 3, next: Some(Box::new(ListNode { val: 4, next: Some(Box::new(ListNode { val: 5, next: None })) })) })) })) }));
+        assert_eq!(l, expected);
+    }
+}
