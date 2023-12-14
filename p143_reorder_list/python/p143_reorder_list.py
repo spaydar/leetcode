5675fbcfd03f403e845e9256322a822a15ac2e37
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-12-13 21:23:34 -0800

    feat: ListNode __eq__ and tests

--- p143_reorder_list/python/p143_reorder_list.py
+++ p143_reorder_list/python/p143_reorder_list.py
@@ -1,3 +1,9 @@
+"""
+LeetCode #143. Reorder List
+
+Reorder a singly-linked list of `n` nodes as follows:
+    `1 > n > 2 > n-1 > 3 > n-2 > ...`
+"""
 from typing import Optional
 
 class ListNode:
@@ -7,6 +13,17 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, ListNode):
+            r1, r2 = self, __value
+            while r1 and r2:
+                if r1.val != r2.val:
+                    return False
+                r1 = r1.next
+                r2 = r2.next
+            return r1 == None and r2 == None
+        return False
 
     def __repr__(self) -> str:
         return f'ListNode({self.val}) -> {self.next}'
@@ -27,7 +44,6 @@ def reorder_list_linear_memory(head: Optional[ListNode]) -> None:
         runner.next = None
         lst.append(runner)
         runner = tmp
-    print('lst', lst)
     runner = ListNode()
     while len(lst) > 0:
         runner.next = lst.pop(0)
@@ -40,11 +56,14 @@ import unittest
 
 class TestReorderList(unittest.TestCase):
     """
-    docs TODO
+    Unit tests for LeetCode 143. Reorder List
     """
 
     @staticmethod
     def create_n_list_nodes(n: int) -> Optional[ListNode]:
+        """
+        Create a linked list whose values increase from 1 to `n`
+        """
         dummy = ListNode()
         runner = dummy
         i = 1
@@ -54,11 +73,54 @@ class TestReorderList(unittest.TestCase):
             i += 1
         return dummy.next
 
-    def test_reorder_list_linear_memory(self):
+    @staticmethod
+    def create_n_list_nodes_reordered(n: int) -> Optional[ListNode]:
+        """
+        Creates a linked list containing `n` nodes reordered as defined in the problem
+        """
+        dummy = ListNode()
+        runner = dummy
+        l, r = 1, n
+        while l <= r:
+            runner.next = ListNode(l)
+            runner = runner.next
+            l += 1
+            if l <= r:
+                runner.next = ListNode(r)
+                runner = runner.next
+                r -= 1
+        return dummy.next
+
+    def test_0_nodes(self):
+        l = self.create_n_list_nodes(0)
+        reorder_list_linear_memory(l)
+        self.assertEqual(l, self.create_n_list_nodes_reordered(0))
+
+    def test_1_nodes(self):
+        l = self.create_n_list_nodes(1)
+        reorder_list_linear_memory(l)
+        self.assertEqual(l, self.create_n_list_nodes_reordered(1))
+
+    def test_4_nodes(self):
         l = self.create_n_list_nodes(4)
-        print('ListNode before', l)
-        self.assertIsNone(reorder_list_linear_memory(l))
-        print('ListNode after', l)
+        reorder_list_linear_memory(l)
+        self.assertEqual(l, self.create_n_list_nodes_reordered(4))
+
+    def test_5_nodes(self):
+        l = self.create_n_list_nodes(5)
+        reorder_list_linear_memory(l)
+        self.assertEqual(l, self.create_n_list_nodes_reordered(5))
+
+    def test_extra_node(self):
+        l = self.create_n_list_nodes(4)
+        reorder_list_linear_memory(l)
+        r = ListNode(0, self.create_n_list_nodes_reordered(4))
+        self.assertNotEqual(l, r)
+
+    def test_not_reordered(self):
+        l = self.create_n_list_nodes(4)
+        reorder_list_linear_memory(l)
+        self.assertNotEqual(l, self.create_n_list_nodes(4))
 
 if __name__ == '__main__':
     unittest.main()
