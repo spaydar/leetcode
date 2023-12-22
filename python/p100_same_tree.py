2518a95bce15ebf0b66efd5c5cb920c4a943d75c
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-12-21 21:51:52 -0800

    feat: p100 same tree in python

---
+++ python/p100_same_tree.py
@@ -0,0 +1,9 @@
+from leetcode_utils import TreeNode
+from typing import Optional
+
+def is_same_tree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if p is None and q is None:
+        return True
+    if p is None or q is None or p.val != q.val:
+        return False
+    return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
