c23481844b21eb1a79213161016b0e8701eb797e
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-12-30 23:13:50 -0800

    feat: p297 serialize and deserialize binary tree in python

--- python/p0297_serialize_and_deserialize_binary_tree.py
+++ python/p0297_serialize_and_deserialize_binary_tree.py
@@ -1,5 +1,5 @@
 from leetcode_utils import TreeNode
-from typing import Optional
+from typing import List, Optional
 
 def serialize(root: Optional[TreeNode]) -> str:
     lst = []
@@ -7,24 +7,21 @@ def serialize(root: Optional[TreeNode]) -> str:
         if not node:
             lst.append('N')
             return
-        lst.append(node.val)
+        lst.append(str(node.val))
         dfs(node.left)
         dfs(node.right)
     dfs(root)
     return ','.join(lst)
 
 def deserialize(data: str) -> Optional[TreeNode]:
-    lst = str.split(',')
-    char = lst.pop(0)
-    root = None if char == 'N' else TreeNode(int(char))
-    def dfs(node: Optional[TreeNode]):
-        # TODO it might be easier to implement this with a TreeNode return type
-        if not lst:
-            return
-        char = lst.pop(0)
-        if char != 'N':
-            node.left = TreeNode(int(char))
-            dfs(node.left)
-        else:
-            node.left = 
+    lst = data.split(',')
+    def dfs(vals: List[str]) -> Optional[TreeNode]:
+        char = vals.pop(0)
+        if char == 'N':
+            return None
+        node = TreeNode(int(char))
+        node.left = dfs(vals)
+        node.right = dfs(vals)
+        return node
+    root = dfs(lst)
     return root
