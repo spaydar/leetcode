a329ebe4464e624fda1c965e07f8416754edb826
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-01-01 15:07:25 -0800

    feat: p78 subsets in python

--- python/p0078_subsets.py
+++ python/p0078_subsets.py
@@ -1,5 +1,16 @@
 from typing import List
 
 def subsets(nums: List[int]) -> List[List[int]]:
-    # TODO
-    return []
+    result = []
+    subset = []
+    def dfs(i: int):
+        if i == len(nums):
+            result.append(subset[:])
+            if len(subset) > 0:
+                subset.pop()
+            return
+        subset.append(nums[i])
+        dfs(i + 1)
+        dfs(i + 1)
+    dfs(0)
+    return result
