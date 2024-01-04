1c126a2cf9c870d1bd15689b545b70034456639e
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-01-03 20:26:24 -0800

    feat: p46 with set in python

--- python/p0046_permutations.py
+++ python/p0046_permutations.py
@@ -3,7 +3,6 @@ from typing import List
 def permutations(nums: List[int]) -> List[List[int]]:
     result = []
     curr = []
-    # On LeetCode, not using a 'visited' set runs faster, perhaps because nums' length is relatively short i.e. <=6 elements
     def dfs():
         if len(curr) == len(nums):
             result.append(curr[:])
@@ -13,5 +12,24 @@ def permutations(nums: List[int]) -> List[List[int]]:
                 curr.append(i)
                 dfs()
                 curr.pop()
+    dfs()
+    return result
+
+# On LeetCode, not using a 'visited' set runs faster, perhaps because nums' length is relatively short i.e. <=6 elements
+def permutations_with_set(nums: List[int]) -> List[List[int]]:
+    result = []
+    curr = []
+    visited = set()
+    def dfs():
+        if len(curr) == len(nums):
+            result.append(curr[:])
+            return
+        for i in nums:
+            if i not in visited:
+                visited.add(i)
+                curr.append(i)
+                dfs()
+                curr.pop()
+                visited.remove(i)
     dfs()
     return result
