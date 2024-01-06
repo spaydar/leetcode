3f7abd17cfecb7ecf16b21aab41bf52f9961ab8f
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-01-05 20:46:29 -0800

    feat p90 subsets ii in python

--- python/p0090_subsets_ii.py
+++ python/p0090_subsets_ii.py
@@ -1,5 +1,18 @@
 from typing import List
 
 def subsets_with_dup(nums: List[int]) -> List[List[int]]:
+    nums.sort()
     result = []
+    curr = []
+    def dfs(i: int):
+        if i == len(nums):
+            result.append(curr[:])
+            return
+        curr.append(nums[i])
+        dfs(i + 1)
+        curr.pop()
+        while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            i += 1
+        dfs(i + 1)
+    dfs(0)
     return result
