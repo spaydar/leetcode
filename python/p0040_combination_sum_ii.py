581827e4483c8abcb9d00396bfc842c3b5acb8e3
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-01-08 21:08:15 -0800

    feat: p40 combination sum ii in python

--- python/p0040_combination_sum_ii.py
+++ python/p0040_combination_sum_ii.py
@@ -1,5 +1,20 @@
 from typing import List
 
 def combination_sum_ii(candidates: List[int], target: int) -> List[List[int]]:
+    candidates.sort()
     result = []
+    curr = []
+    def dfs(i: int, total: int):
+        if total == target:
+            result.append(curr[:])
+            return
+        if i == len(candidates) or total > target:
+            return
+        curr.append(candidates[i])
+        dfs(i + 1, total + candidates[i])
+        curr.pop()
+        while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+            i += 1
+        dfs(i + 1, total)
+    dfs(0, 0)
     return result
