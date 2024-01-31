1c9dfbc8768c66a245354b4d3028c2a973b68ebb
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-01-30 21:51:16 -0800

    feat: p91 num decodings, table and constant sols in python

--- python/p0091_decode_ways.py
+++ python/p0091_decode_ways.py
@@ -17,5 +17,23 @@ def num_decodings_memo(s: str) -> int:
         return memo[i]
     return dfs(0)
 
+def num_decodings_table(s: str) -> int:
+    table = [0 for _ in range(len(s) + 2)]
+    table[1] = 1
+    for i in range(0, len(s)):
+        t = i + 2
+        table[t] = 0 if s[i] == '0' else table[t - 1]
+        if i > 0 and (s[i - 1] == '1' or (s[i - 1] == '2' and s[i] < '7')):
+            table[t] += table[t - 2]
+    return table[-1]
+
 def num_decodings_constant(s: str) -> int:
-    # TODO
+    prev, curr = 0, 1
+    i = 0
+    while i < len(s):
+        next = 0 if s[i] == '0' else curr
+        if i > 0 and (s[i - 1] == '1' or (s[i - 1] == '2' and s[i] < '7')):
+            next += prev
+        prev, curr = curr, next
+        i += 1
+    return curr
