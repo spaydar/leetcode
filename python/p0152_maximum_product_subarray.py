3fb40f13030a37aef9e794e1567b50f6f7dc1447
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-02-02 22:00:29 -0800

    feat: p152 maximum product subarray in python

--- python/p0152_maximum_product_subarray.py
+++ python/p0152_maximum_product_subarray.py
@@ -1,5 +1,11 @@
 from typing import List
 
-def max_product(nums: List[int]) -> int:
-    # TODO
-    return -1
+def max_product_memo(nums: List[int]) -> int:
+    result = nums[0]
+    high, low = 1, 1
+    for n in nums:
+        tmp = high
+        high = max(n * high, n * low, n)
+        low = min(n * tmp, n * low, n)
+        result = max(result, high)
+    return result
