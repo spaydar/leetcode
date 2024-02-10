8001742d7b7ed55b3f4a177e4bc8bf987de3e2fd
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-02-09 21:21:11 -0800

    feat: p1 two sum in python

--- python/p0001_two_sum.py
+++ python/p0001_two_sum.py
@@ -1,5 +1,10 @@
 from typing import List
 
 def two_sum(nums: List[int], target: int) -> List[int]:
-    # TODO
-    return []
+    value_indices_dict = {}
+    for i, val in enumerate(nums):
+        diff = target - val
+        if diff in value_indices_dict:
+            return [value_indices_dict[diff], i]
+        value_indices_dict[val] = i
+    return [-1, -1]
