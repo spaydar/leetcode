79461c7e4a29468eaf7a3a9d6289b408b862a112
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-02-18 10:44:06 -0800

    feat: p295 heap approaches in pythong

--- python/p0295_find_median_from_data_stream.py
+++ python/p0295_find_median_from_data_stream.py
@@ -1,3 +1,4 @@
+from heapq import heappop, heappush, heappushpop
 from typing import List
 
 class MedianFinderBinarySearch:
@@ -28,10 +29,40 @@ class MedianFinderBinarySearch:
 class MedianFinderHeap:
 
     def __init__(self) -> None:
-        pass
+        self.max_heap = [] # Smaller half stored in max heap
+        self.min_heap = [] # Larger half stored in min heap
 
     def add_num(self, num: int) -> None:
-        pass
+        heappush(self.max_heap, num * -1)
+
+        if self.min_heap and self.max_heap[0] * -1 > self.min_heap[0]:
+            heappush(self.min_heap, heappop(self.max_heap) * -1)
+
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heappush(self.min_heap, heappop(self.max_heap) * -1)
+        elif len(self.min_heap) > len(self.max_heap) + 1:
+            heappush(self.max_heap, heappop(self.min_heap) * -1)
 
     def find_median(self) -> float:
-        return 0.0
+        if len(self.max_heap) > len(self.min_heap):
+            return self.max_heap[0] * -1
+        if len(self.max_heap) < len(self.min_heap):
+            return self.min_heap[0]
+        return (self.max_heap[0] * -1 + self.min_heap[0]) / 2
+
+class MedianFinderHeapPushPop:
+    # heappushpop performs better than separate calls to heappush and heappop as per heapq docs
+    def __init__(self) -> None:
+        self.small = []
+        self.large = []
+
+    def add_num(self, num: int) -> None:
+        if len(self.small) == len(self.large):
+            heappush(self.small, heappushpop(self.large, num) * -1)
+        else:
+            heappush(self.large, heappushpop(self.small, num * -1) * -1)
+
+    def find_median(self) -> float:
+        if len(self.small) == len(self.large):
+            return (self.large[0] - self.small[0]) / 2
+        return self.small[0] * -1
