a69c62bf756fdf40359d4cfa0b8db149e5268367
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-02-18 20:22:07 -0800

    feat: p62 2d dp and memory optimization for 1d in python

--- python/p0062_unique_paths.py
+++ python/p0062_unique_paths.py
@@ -10,7 +10,18 @@ def unique_paths_memo(m: int, n: int) -> int:
         return memo[(x, y)]
     return dfs(0, 0)
 
-def unique_paths_table(m: int, n: int) -> int:
+def unique_paths_table_2d(m: int, n: int) -> int:
+    dp = [[0 for _ in range(n + 1)] for _ in range(m)]
+    for i in range(1, n + 1):
+        dp[0][i] = 1
+    for i in range(1, m):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[-1][-1]
+
+def unique_paths_table_1d(m: int, n: int) -> int:
+    if n > m:
+        m, n = n, m
     table = [1 for _ in range(n)]
     for _ in range(m - 1):
         for i in range(1, n):
