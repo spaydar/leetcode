49c334789ca7d8bce2ab2129b9a69616010b4767
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-03 10:28:52 -0800

    p0704_binary_search.py

---
+++ python/p0704_binary_search.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def search(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = l + (r - l) // 2
+        if nums[m] == target:
+            return m
+        if nums[m] < target:
+            l = m + 1
+        else:
+            r = m - 1
+    return -1
