fd064bc91c963dea6f0e72b736bd41b8a14e7087
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-05 08:46:42 -0800

    p0033_search_in_rotated_sorted_array.py

---
+++ python/p0033_search_in_rotated_sorted_array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def search(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = l + (r - l) // 2
+        if nums[m] == target:
+            return m
+        if nums[m] > nums[r]:
+            if nums[m] < target or nums[l] > target:
+                l = m + 1
+            else:
+                r = m - 1
+        else:
+            if nums[m] > target or nums[r] < target:
+                r = m - 1
+            else:
+                l = m + 1
+    return -1
