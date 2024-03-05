10633b376dbdcc64364cedca53a3a1ed02c01bdc
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-05 09:34:05 -0800

    p0981_time_based_key_value_store.py

---
+++ python/p0981_time_based_key_value_store.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from typing import DefaultDict, List, Tuple
+
+class TimeMap:
+
+    def __init__(self) -> None:
+        self.data: DefaultDict[str, List[Tuple[int, str]]] = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.data[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if not key in self.data or self.data[key][0][0] > timestamp:
+            return ''
+        l, r = 0, len(self.data[key]) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if self.data[key][m][0] == timestamp:
+                return self.data[key][m][1]
+            if self.data[key][m][0] < timestamp:
+                l = m + 1
+            else:
+                r = m - 1
+        return self.data[key][r][1]
