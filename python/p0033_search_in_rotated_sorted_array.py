56f1e457f9c91fd944eb9748ea1c3b21d9e7be40
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-05 08:55:17 -0800

    feat: intuitive if conditions p33 in python

--- python/p0033_search_in_rotated_sorted_array.py
+++ python/p0033_search_in_rotated_sorted_array.py
@@ -17,3 +17,21 @@ def search(nums: List[int], target: int) -> int:
             else:
                 l = m + 1
     return -1
+
+def search_intuitive_if_conds(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = l + (r - l) // 2
+        if nums[m] == target:
+            return m
+        if nums[m] > nums[r]:
+            if nums[l] <= target < nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+        else:
+            if nums[m] < target <= nums[r]:
+                l = m + 1
+            else:
+                r = m - 1
+    return -1
