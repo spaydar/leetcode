19a84ed33a5be7f2917baf22a290c198457f25fc
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-05 09:43:36 -0800

    feat: p981 optimization in python

--- python/p0981_time_based_key_value_store.py
+++ python/p0981_time_based_key_value_store.py
@@ -22,3 +22,20 @@ class TimeMap:
             else:
                 r = m - 1
         return self.data[key][r][1]
+
+    def get_optimized(self, key: str, timestamp: int) -> str:
+        if not key in self.data or self.data[key][0][0] > timestamp:
+            return ''
+        # Checking the last element before searching is not necessary but provides significat improvement on LeetCode
+        if self.data[key][-1][0] <= timestamp:
+            return self.data[key][-1][1]
+        l, r = 0, len(self.data[key]) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if self.data[key][m][0] == timestamp:
+                return self.data[key][m][1]
+            if self.data[key][m][0] < timestamp:
+                l = m + 1
+            else:
+                r = m - 1
+        return self.data[key][r][1]
