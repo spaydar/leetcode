0a097909a6455be2dd14e88e2f15da87320d3fb6
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-09 15:20:49 -0800

    p0138_copy_list_with_random_pointer.py

---
+++ python/p0138_copy_list_with_random_pointer.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+def copy_random_list(head: Optional[Node]) -> Optional[Node]:
+    node_map = { None: None }
+    runner = head
+    while runner:
+        node_map[runner] = Node(runner.val)
+        runner = runner.next
+    runner = head
+    while runner:
+        node_map[runner].next = node_map[runner.next]
+        node_map[runner].random = node_map[runner.random]
+        runner = runner.next
+    return node_map[head]
