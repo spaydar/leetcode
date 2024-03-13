a4080aece68b1425ea460bd72ac1514d1e5ba519
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-12 21:46:28 -0700

    p23 mergesort approach in python

--- python/p0023_merge_k_sorted_lists.py
+++ python/p0023_merge_k_sorted_lists.py
@@ -19,5 +19,30 @@ def merge_k_lists_heap(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
     return dummy.next
 
 def merge_k_lists_mergesort(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-    # TODO
-    pass
+    def merge_two_lists(a: Optional[ListNode], b: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = cur = ListNode()
+        while a and b:
+            if a.val < b.val:
+                cur.next = a
+                a = a.next
+            else:
+                cur.next = b
+                b = b.next
+            cur = cur.next
+        if a:
+            cur.next = a
+        if b:
+            cur.next = b
+        return dummy.next
+    def mergesort(start: int, end: int) -> Optional[ListNode]:
+        if start > end:
+            return None
+        if start == end:
+            return lists[start]
+        if start + 1 == end:
+            return merge_two_lists(lists[start], lists[end])
+        mid = start + (end - start) // 2
+        left = mergesort(start, mid)
+        right = mergesort(mid + 1, end)
+        return merge_two_lists(left, right)
+    return mergesort(0, len(lists) - 1)
