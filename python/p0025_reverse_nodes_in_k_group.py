2c46dd293415c065f79f331b9a3e68c888009e24
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-15 20:42:48 -0700

    refactor p25 in python

--- python/p0025_reverse_nodes_in_k_group.py
+++ python/p0025_reverse_nodes_in_k_group.py
@@ -2,12 +2,13 @@ from leetcode_utils import ListNode
 from typing import Optional
 
 def reverse_k_group(head: Optional[ListNode], k: int) -> Optional[ListNode]:
-    def reverse_segment(prev: Optional[ListNode], first: Optional[ListNode], last: Optional[ListNode]) -> Optional[ListNode]:
-        while first is not last:
-            next = first.next
-            first.next = prev
-            prev = first
-            first = next
+    def reverse_segment(cur: Optional[ListNode], next_group: Optional[ListNode]) -> Optional[ListNode]:
+        prev = next_group
+        while cur is not next_group:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
         return prev
     dummy = prev_group = ListNode(0, head)
     next_group = head
@@ -18,7 +19,7 @@ def reverse_k_group(head: Optional[ListNode], k: int) -> Optional[ListNode]:
                 return dummy.next
             next_group = next_group.next
             count += 1
-        prev_group.next = reverse_segment(next_group, head, next_group)
+        prev_group.next = reverse_segment(head, next_group)
         head.next = next_group
         prev_group = head
         head = next_group
