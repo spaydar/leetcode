6de4e5238b8c357cd26fa86d18942f772699f5e9
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-15 20:35:43 -0700

    p0025_reverse_nodes_in_k_group.py

--- python/p0025_reverse_nodes_in_k_group.py
+++ python/p0025_reverse_nodes_in_k_group.py
@@ -2,21 +2,24 @@ from leetcode_utils import ListNode
 from typing import Optional
 
 def reverse_k_group(head: Optional[ListNode], k: int) -> Optional[ListNode]:
-    # TODO this doesn't work yet
     def reverse_segment(prev: Optional[ListNode], first: Optional[ListNode], last: Optional[ListNode]) -> Optional[ListNode]:
-        while first is not last.next:
+        while first is not last:
             next = first.next
             first.next = prev
             prev = first
             first = next
-        return last
-    dummy = slow = fast = ListNode(0, head)
-    count = 0
-    while fast:
-        while count < k:
-            if not fast:
-                return dummy.next
-            fast = fast.next
-            count += 1
+        return prev
+    dummy = prev_group = ListNode(0, head)
+    next_group = head
+    while next_group:
         count = 0
-        # TODO reverse segment
+        while count < k:
+            if not next_group:
+                return dummy.next
+            next_group = next_group.next
+            count += 1
+        prev_group.next = reverse_segment(next_group, head, next_group)
+        head.next = next_group
+        prev_group = head
+        head = next_group
+    return dummy.next
