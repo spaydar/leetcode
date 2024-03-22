93df27c40df4c412d1131e786bc44166c76a599e
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-22 13:02:33 -0700

    p167 simplified branches in python

--- python/p0167_two_sum_ii.py
+++ python/p0167_two_sum_ii.py
@@ -10,3 +10,12 @@ def two_sum(numbers: List[int], target: int) -> List[int]:
             left += 1
         else:
             right -= 1
+
+def two_sum_simplified(numbers: List[int], target: int) -> List[int]:
+    left, right = 0, len(numbers) - 1
+    while numbers[left] + numbers[right] != target:
+        if numbers[left] + numbers[right] < target:
+            left += 1
+        else:
+            right -= 1
+    return [left + 1, right + 1]
