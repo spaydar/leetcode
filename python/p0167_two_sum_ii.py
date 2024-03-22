fa91532a19e4cf824dbfb947196df255a98c52d5
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-22 12:51:58 -0700

    p0167_two_sum_ii.py

--- python/p0167_two_sum_ii.py
+++ python/p0167_two_sum_ii.py
@@ -1,5 +1,12 @@
 from typing import List
 
 def two_sum(numbers: List[int], target: int) -> List[int]:
-    # TODO
-    return []
+    left, right = 0, len(numbers) - 1
+    while True:
+        val = numbers[left] + numbers[right]
+        if val == target:
+            return [left + 1, right + 1]
+        if val < target:
+            left += 1
+        else:
+            right -= 1
