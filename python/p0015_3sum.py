92297bbb5932b19b10c3ffb0ef4ce588cc065e05
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-03-24 23:57:42 -0700

    p0015_3sum.py

--- python/p0015_3sum.py
+++ python/p0015_3sum.py
@@ -1,32 +1,45 @@
 from typing import List
 
 def three_sum(nums: List[int]) -> List[List[int]]:
-    # TODO this isn't working yet
-    # consider case where a target number isn't found -- we shouldn't increment both pointers
     result = []
     nums.sort()
-    def binary_search(start: int, end: int, target: int) -> int:
-        while start <= end:
-            mid = start + (end - start) // 2
-            if nums[mid] < target:
-                start = mid + 1
-            elif nums[mid] > target:
-                end = mid - 1
+    i = 0
+    while i < len(nums) - 2:
+        j, k = i + 1, len(nums) - 1
+        while j < k:
+            val = nums[i] + nums[j] + nums[k]
+            if val < 0:
+                j += 1
+            elif val > 0:
+                k -= 1
             else:
-                return mid
-        return -1
-    left, right = 0, len(nums) - 1
-    while left < right:
-        idx = binary_search(left + 1, right - 1, 0 - (nums[left] + nums[right]))
-        if idx != -1:
-            result.append([nums[left], nums[idx], nums[right]])
-        left += 1
-        right -= 1
-        while (
-                left < len(nums) and 
-                right > -1 and
-                nums[left] == nums[left - 1] and
-                nums[right] == nums[right + 1]
-        ):
-            right -= 1
+                result.append([nums[i], nums[j], nums[k]])
+                j += 1
+                while j < k and nums[j] == nums[j - 1]:
+                    j += 1
+        i += 1
+        while i < len(nums) and nums[i] == nums[i - 1]:
+            i += 1
+    return result
+
+def three_sum_always_skip_repeated_nums(nums: List[int]) -> List[List[int]]:
+    result = []
+    nums.sort()
+    i = 0
+    while i < len(nums) - 2:
+        j, k = i + 1, len(nums) - 1
+        while j < k:
+            val = nums[i] + nums[j] + nums[k]
+            if val == 0:
+                result.append([nums[i], nums[j], nums[k]])
+                val -= 1
+            if val < 0:
+                j += 1
+                while j < k and nums[j] == nums[j - 1]:
+                    j += 1
+            elif val > 0:
+                k -= 1
+        i += 1
+        while i < len(nums) and nums[i] == nums[i - 1]:
+            i += 1
     return result
