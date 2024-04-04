0f0526810bed2939743db6d4485eda44bbd30d3f
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2024-04-03 18:03:59 -0700

    p0011_container_with_most_water.py

--- python/p0011_container_with_most_water.py
+++ python/p0011_container_with_most_water.py
@@ -1,5 +1,12 @@
 from typing import List
 
 def max_area(height: List[int]) -> int:
-    # TODO
-    return -1
+    l, r = 0, len(height) - 1
+    result = 0
+    while l < r:
+        result = max(result, (r - l) * min(height[l], height[r]))
+        if height[r] < height[l]:
+            r -= 1
+        else:
+            l += 1
+    return result
